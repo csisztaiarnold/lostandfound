7188f83af7cb5f2180d59f45d067e2b64a559b1f
Csisztai Arnold <csisztai.arnold@gmail.com>
2017-10-28 15:46:42 +0200

    added jQuery

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/vendor/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}?r=<?php echo rand(); ?>" rel="stylesheet">
-    <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </head>
 <body>
 
